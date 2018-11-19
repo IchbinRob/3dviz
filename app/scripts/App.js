3373f4820620bcc820462f388affbf9ebd88f8f5
LACOTTE Robinson <rlacotte@g-a310-01.intra.gobelins.fr>
2018-11-19 16:56:57 +0100

    initial commit

--- app/scripts/App.js
+++ app/scripts/App.js
@@ -7,11 +7,6 @@
 
 import Sound from './Sound'
 //Assets
-import imgTexture from '../assets/Rock_025_COLOR.jpg';
-import imgNormal from '../assets/Rock_025_NORM.jpg';
-import imgDisp from '../assets/Rock_025_DISP.png';
-import imgRoughness from '../assets/Rock_025_ROUGH.jpg';
-import imgAo from '../assets/Rock_025_OCC.jpg';
 
 // 126
 import SrcMusic from '../assets/Blaz - Steven - 08 Battle Of Steven.mp3'
@@ -26,29 +21,26 @@ export default class App {
 
         this.sound = new SoundController()
 
-        let textureLoader = new THREE.TextureLoader();
-        let texture = textureLoader.load(imgTexture);
-        let normal = textureLoader.load(imgNormal);
-        let disp = textureLoader.load(imgDisp)
-        let roughnessmap = textureLoader.load(imgRoughness)
-        let ao = textureLoader.load(imgAo)
+        // let textureLoader = new THREE.TextureLoader();
+        // let texture = textureLoader.load(imgTexture);
+
 
         this.camera = new THREE.PerspectiveCamera( 150, window.innerWidth / window.innerHeight, 0.1, 10 );
         this.camera.position.z = 1;
 
     	this.scene = new THREE.Scene();
-        let material = new THREE.MeshStandardMaterial({
-            roughness: .3, metalness: 0,
-            color: 0xffffff,
-            map: texture,
-            normalMap: normal,
-            normalScale: new THREE.Vector2(5, 5),
-            displacementMap: disp,
-            displacementScale: 0.05,
-            roughnessMap: roughnessmap,
-            aoMap: ao,
-            aoMapIntensity: 2
-        });
+        // let material = new THREE.MeshStandardMaterial({
+        //     roughness: .3, metalness: 0,
+        //     color: 0xffffff,
+        //     map: texture,
+        //     normalMap: normal,
+        //     normalScale: new THREE.Vector2(5, 5),
+        //     displacementMap: disp,
+        //     displacementScale: 0.05,
+        //     roughnessMap: roughnessmap,
+        //     aoMap: ao,
+        //     aoMapIntensity: 2
+        // });
 
         let ambLight = new THREE.AmbientLight(0xffffff)
         this.scene.add(ambLight)
@@ -57,24 +49,6 @@ export default class App {
         let dirLight = new THREE.DirectionalLight(0xffffff, 0.2)
         this.scene.add(dirLight)
 
-        this.mesh = []
-        for (let i = 0; i < 1; i++) {
-            let geometry
-            if (i % 2 == 0) {
-                geometry = new THREE.SphereGeometry(0.8, 50, 50);
-            } else {
-                geometry = new THREE.ConeGeometry(Math.random() * 0.5, Math.random() * 0.5, Math.random() * 0.5);
-            }
-            
-            this.mesh[i] = new THREE.Mesh( geometry, material );
-            this.scene.add( this.mesh[i] );
-            this.mesh[i].position.x = i * 0.5
-            this.mesh[i].position.y = i * 0.5
-            //this.mesh[i].position.z = i * 0.5
-            ///this.mesh[i].scale.set(-1,-1,-1)
-            //this.mesh[i].position.z = i * 0.25
-            
-        }
 
     	this.renderer = new THREE.WebGLRenderer( { antialias: true } );
     	this.renderer.setPixelRatio( window.devicePixelRatio );
